ecf6b69075dfb6f5eaf0e0c4736576e1e77b1727
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-12 10:34:25 +0100

    Experience card components

---
+++ resources/views/components/cards/experienceCard.blade.php
@@ -0,0 +1,31 @@
+@props([
+    'href' => '/',
+    'target' => '_self',
+    'title',
+    'tenure',
+    'description',
+    'technologies' => '',
+])
+
+<a href="{{ $href }}" target="{{ $target }}" class="block" aria-label="Experience card">
+    <div
+        class="group hover:bg-darkPurple-600 rounded-2xl transition-all duration-300 ease-in-out hover:outline hover:outline-white lg:p-5"
+    >
+        <div class="space-y-6 lg:space-y-4">
+            <div class="flex flex-col-reverse justify-between lg:flex-row">
+                <h3 class="group-hover:text-violet text-white group-hover:font-bold">{{ $title }}</h3>
+                <span>{{ $tenure }}</span>
+            </div>
+            <p class="mt-2 text-sm leading-normal">
+                {{ $description }}
+            </p>
+        </div>
+        <ul class="mt-6 flex flex-wrap" aria-label="Technologies used">
+            @foreach (explode(',', $technologies) as $technology)
+                <li class="mt-2 mr-1.5">
+                    <x-pill>{{ $technology }}</x-pill>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</a>
---
+++ resources/views/components/experience.blade.php
@@ -0,0 +1,29 @@
+<section id="experience" class="my-16 space-y-8 md:mb-24 lg:my-26" aria-label="Work experience">
+    <div
+        class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+    >
+        <h2 class="text-sm font-bold tracking-widest text-slate-200 uppercase lg:sr-only">Experience</h2>
+    </div>
+    <x-cards.experienceCard
+        href="https://forthwithlife.co.uk/"
+        target="_blank"
+        title="Web Developer - Forth"
+        tenure="2022 - present"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+        technologies="Laravel, TailwindCSS, Vue, React, PHP"
+    />
+    <x-cards.experienceCard
+        href="https://futurehealthbiobank.com/"
+        target="_blank"
+        title="Frontend Developer - Future health biobank"
+        tenure="2021 - 2022"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+        technologies="Laravel, TailwindCSS, Vue, React, PHP"
+    />
+    <x-cards.experienceCard
+        title="Freelance Developer"
+        tenure="2024 - present"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+        technologies="Laravel, TailwindCSS, Vue, React, PHP"
+    />
+</section>
