3d4febb386f82a3199e0ce662c7b402c5cb72662
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-12 10:34:01 +0100

    Fix image sizing and add in before element

--- resources/views/components/hero/image.blade.php
+++ resources/views/components/hero/image.blade.php
@@ -1,7 +1,11 @@
-<div>
+<div
+    class="relative w-fit before:absolute before:-top-5 before:-left-5 before:-z-1 before:size-full before:rounded-[50px_10px] before:bg-white before:content-['']"
+>
     <img
+        width="400"
+        height="400"
         src="{{ asset('images/CarterStevens.png') }}"
         alt="Carter Stevens in amsterdam"
-        class="size-fit rounded-[50px_10px] max-md:max-h-[300px] max-md:max-w-[225px]"
+        class="mx-auto size-[400px] rounded-[50px_10px] object-cover max-md:max-h-[300px] max-md:max-w-[225px]"
     />
 </div>
