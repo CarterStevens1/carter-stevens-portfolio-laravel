ea44a0d19dffd75186b7813927ba34ea5bf1e5f6
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-12 10:34:17 +0100

    add in basic pill and link components

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes(['class' => 'font-medium text-slate-200 hover:text-white focus-visible:text-white']) }}>
+    {{ $slot }}
+</a>
---
+++ resources/views/components/pill.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-violet flex items-center rounded-full px-3 py-1 text-xs leading-5 font-medium text-white">
+    {{ $slot }}
+</div>
