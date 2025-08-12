e16adea67532aa59434a88ddeae5aa4bd7d56d1b
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-12 10:33:38 +0100

    Add files for social icons display

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,5 @@
+<li class="mr-5 shrink-0 text-xs">
+    <a {{ $attributes(['class' => 'block text-[#B9B9B9] hover:text-white']) }}>
+        {{ $slot }}
+    </a>
+</li>
---
+++ resources/views/components/socialLinks.blade.php
@@ -0,0 +1,52 @@
+<ul class="mt-8 ml-1 flex items-center" aria-label="Social Links">
+    <x-icon
+        href="Linked"
+        title="Discord"
+        rel="noreferrer noopener"
+        target="_blank"
+        aria-lable="Linked in (opens in new tab)"
+    >
+        <span class="sr-only">Linked in</span>
+        <x-svgs.linkedin />
+    </x-icon>
+    <x-icon
+        href="github"
+        title="Github"
+        rel="noreferrer noopener"
+        target="_blank"
+        aria-lable="Github (opens in new tab)"
+    >
+        <span class="sr-only">Github</span>
+        <x-svgs.github />
+    </x-icon>
+    <x-icon
+        href="discord"
+        title="Instagram"
+        rel="noreferrer noopener"
+        target="_blank"
+        aria-lable="Instagram (opens in new tab)"
+    >
+        <span class="sr-only">Instagram</span>
+        <x-svgs.instagram />
+    </x-icon>
+    <x-icon
+        href="discord"
+        title="Discord"
+        rel="noreferrer noopener"
+        target="_blank"
+        aria-lable="Discord (opens in new tab)"
+    >
+        <span class="sr-only">Discord</span>
+        <x-svgs.discord />
+    </x-icon>
+    <x-icon
+        href="discord"
+        title="Email"
+        rel="noreferrer noopener"
+        target="_blank"
+        aria-lable="Email (opens in new tab)"
+    >
+        <span class="sr-only">Email</span>
+        <x-svgs.envelope />
+    </x-icon>
+</ul>
