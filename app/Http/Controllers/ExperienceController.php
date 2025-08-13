93d85ce3466383a88f5b9b2b78c514f9993fa70b
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-13 11:55:22 +0100

    create initial migration model and factory for job experience

---
+++ app/Http/Controllers/ExperienceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExperienceController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request) {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
---
+++ app/Models/Experience.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Experience extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExperienceFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'job_title',
+        'company',
+        'start_date',
+        'end_date',
+        'description',
+        'skills_used',
+    ];
+}
