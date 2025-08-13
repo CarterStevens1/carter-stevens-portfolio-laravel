da6d521115518b3e39922290f2e35ff051ba8979
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-13 14:17:40 +0100

    Add social urls

--- resources/views/components/socialLinks.blade.php
+++ resources/views/components/socialLinks.blade.php
@@ -1,6 +1,6 @@
 <ul class="mt-8 ml-1 flex items-center" aria-label="Social Links">
     <x-icon
-        href="Linked"
+        href="https://www.linkedin.com/in/carter-stevens-2a6382208"
         title="Discord"
         rel="noreferrer noopener"
         target="_blank"
@@ -10,7 +10,7 @@
         <x-svgs.linkedin />
     </x-icon>
     <x-icon
-        href="github"
+        href="https://github.com/CarterStevens1"
         title="Github"
         rel="noreferrer noopener"
         target="_blank"
@@ -19,28 +19,32 @@
         <span class="sr-only">Github</span>
         <x-svgs.github />
     </x-icon>
-    <x-icon
+    {{--
+        <x-icon
         href="discord"
         title="Instagram"
         rel="noreferrer noopener"
         target="_blank"
         aria-lable="Instagram (opens in new tab)"
-    >
+        >
         <span class="sr-only">Instagram</span>
         <x-svgs.instagram />
-    </x-icon>
-    <x-icon
+        </x-icon>
+    --}}
+    {{--
+        <x-icon
         href="discord"
         title="Discord"
         rel="noreferrer noopener"
         target="_blank"
         aria-lable="Discord (opens in new tab)"
-    >
+        >
         <span class="sr-only">Discord</span>
         <x-svgs.discord />
-    </x-icon>
+        </x-icon>
+    --}}
     <x-icon
-        href="discord"
+        href="mailto:carter1509@protonmail.com"
         title="Email"
         rel="noreferrer noopener"
         target="_blank"
