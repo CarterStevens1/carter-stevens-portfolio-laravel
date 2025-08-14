64bbe2ca4e2184b797ad8f08db7dc0ceba370f47
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-14 12:37:41 +0100

    update the teal color to the theme colors to match

--- resources/views/components/dashboardNav.blade.php
+++ resources/views/components/dashboardNav.blade.php
@@ -13,7 +13,7 @@
         <div>
             <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
                 <span class="block text-slate-200">Welcome</span>
-                <span class="block text-teal-300">{{ Auth::user()->name }}</span>
+                <span class="text-violet block">{{ Auth::user()->name }}</span>
             </h1>
             <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-400 sm:text-xl">{{ $pageName }}</h2>
             <p class="mt-6 text-base leading-7 text-slate-400">
