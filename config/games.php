7496f489e5af2749eefca36160479fc392840d46
Carter Stevens <carter@forthwithlife.co.uk>
2025-09-03 09:45:55 +0100

    Add extra games

--- config/games.php
+++ config/games.php
@@ -13,7 +13,23 @@ return [
     308958, // AC 2
     237701, // AC: 3
     5606, // AC: UNITY
+    113, // AC: Brotherhood
     132516, //Phasmophobia
+    251833, //Balatro
+    17000, // Stardew Valley
+    72, // Portal 2
+    124, //Left for dead 2
+    11133, //Dark souls 3
+    472, //Skyrim
+    9630, //Fallout 4
+    9730, // Firewatch
+    3042, // Dying Light
+    11156, //Horizon zero dawn
+    7323, // Rise of the Tomb Raider
+    37777, // Shadow of the tomb raider
+    9174, // Little nightmares
+    121760, // Little nightmares 2
+    15, // Fallout 3
     // Add more IGDB game IDs as needed
   ],
 ];
