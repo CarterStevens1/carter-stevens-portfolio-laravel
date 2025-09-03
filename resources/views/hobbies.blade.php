b89ae52da71b196cf9939fc6f3d415cf38c569c4
Carter Stevens <carter@forthwithlife.co.uk>
2025-09-03 15:34:21 +0100

    Add an intro area to the hobbies page

--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -11,9 +11,53 @@
             <x-hero.mainHero />
         </header>
         <main class="flex flex-col items-center py-12 lg:w-[60%]">
-            <section id="played-games" class="relative w-full">
-                <h2>Top Games</h2>
+            <section>
+                <div class="mb-16 scroll-mt-16 space-y-8 md:mb-24">
+                    <div
+                        id="about"
+                        class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+                    >
+                        <h2 class="text-sm font-bold tracking-widest text-slate-200 uppercase lg:sr-only">About me</h2>
+                    </div>
+                    <x-textBlock.text>
+                        Here you can find a bit more about me as a person, my interests and what I do in my spare time.
+                        I grew up loving video games and this has stuck with me ever since. My range of games is not
+                        really limited and I generally play whatever looks good, ranging from cosy simulators to full
+                        blown action adventure games.
+                    </x-textBlock.text>
+                    <x-textBlock.text>
+                        Below you can find some of my favourite games with a link to my Backloggd page containing all
+                        the games I have
+                        <strong class="italic underline">EVER</strong>
+                        played.
+                        <small class="block">(minus the ones I can't remember)</small>
+                    </x-textBlock.text>
 
+                    <x-textBlock.text>
+                        If I am not gaming you can usually find me, watching youtube, reading, or probably watching
+                        horror movies with my partner.
+                        <small class="block">
+                            (I never used to like horror movies until I had to endure them everyday but now I love them)
+                        </small>
+                    </x-textBlock.text>
+
+                    <x-textBlock.text>
+                        Down below you can find some of the recent horror movies I have watched.
+                    </x-textBlock.text>
+
+                    <x-textBlock.text>
+                        Other than that, I am just a normal guy who likes a good cider on the weekend and relaxing.
+                        <small class="block">(Bonus: Some photos of my dog at the bottom of the page)</small>
+                    </x-textBlock.text>
+                </div>
+            </section>
+            <section id="topGames" class="relative w-full">
+                <div
+                    id="top-games"
+                    class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+                >
+                    <h2 class="text-sm font-bold tracking-widest text-slate-200 uppercase lg:sr-only">Top Games</h2>
+                </div>
                 @if (count($games) > 0)
                     <div class="mt-8 grid grid-cols-2 gap-6 md:grid-cols-2 lg:grid-cols-4 lg:gap-12">
                         @foreach ($games as $game)
