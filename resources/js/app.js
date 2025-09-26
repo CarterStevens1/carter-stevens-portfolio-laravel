c6b41a9365ffa8c387b8ef3d3b48867f1ac42b86
Carter Stevens <carter@forthwithlife.co.uk>
2025-09-26 13:58:02 +0100

    JS to open the image on click for the cards

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,3 +15,23 @@ document.addEventListener('mousemove', updateCursorPositionv2);
 spotlightContainer.style.setProperty('--mouse-x', '10%');
 spotlightContainer.style.setProperty('--mouse-y', '10%');
 
+
+
+// Modal JS
+
+let dialog = document.querySelector('dialog');
+let card = document.querySelectorAll('div.hobbieCard');
+
+for (let i = 0, len = card.length; i < len; i++) {
+  card[i].addEventListener('click', function () {
+    dialog.showModal();
+    dialog.querySelector('img').src = this.querySelector('img').src;
+  });
+}
+
+window.addEventListener('click', function (event) {
+  if (event.target == dialog) {
+    dialog.close();
+  }
+});
+
