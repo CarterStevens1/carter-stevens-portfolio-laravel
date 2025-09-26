c9f8a08a3adf8e411478b859e0b216a54019e3a8
Carter Stevens <carter@forthwithlife.co.uk>
2025-09-26 13:58:17 +0100

    Add joint class to allow the JS to target each card

--- resources/views/components/cards/movieCard.blade.php
+++ resources/views/components/cards/movieCard.blade.php
@@ -3,7 +3,7 @@
     'title' => '',
 ])
 <div
-    class="group relative max-w-fit rounded-lg transition-all duration-200 ease-in-out before:transition-all before:duration-200 before:ease-in-out hover:scale-105 hover:outline hover:outline-white hover:before:absolute hover:before:h-full hover:before:w-full hover:before:rounded-lg hover:before:bg-black/50 hover:before:content-['']"
+    class="group hobbieCard relative max-w-fit rounded-lg transition-all duration-200 ease-in-out before:transition-all before:duration-200 before:ease-in-out hover:scale-105 hover:outline hover:outline-white hover:before:absolute hover:before:h-full hover:before:w-full hover:before:rounded-lg hover:before:bg-black/50 hover:before:content-['']"
 >
     <img class="max-h-50 max-w-40 rounded-lg object-contain" src="{{ asset($image) }}" alt="{{ $title }}" />
     <span
