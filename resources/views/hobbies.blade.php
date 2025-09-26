6ff25da887499fecf1c35f3e28b2d39229179108
Carter Stevens <carter@forthwithlife.co.uk>
2025-09-26 13:57:53 +0100

    Add a dialog that opens the clicked image

--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -21,4 +21,10 @@
             <x-hobbies.dogPhotos />
         </main>
     </div>
+
+    <dialog
+        class="inset-1/2 -translate-x-1/2 -translate-y-1/2 scale-0 bg-transparent opacity-0 transition-all duration-300 ease-linear open:block open:scale-100 open:opacity-100 starting:open:scale-0 starting:open:opacity-0"
+    >
+        <img class="max-h-100 max-w-80 rounded-lg object-contain" src="" alt="" />
+    </dialog>
 </x-layout>
